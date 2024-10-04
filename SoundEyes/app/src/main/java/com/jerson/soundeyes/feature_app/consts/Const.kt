3bf3f850a73ff5d482fc480fcfad6f723b3a0809
Jerson Vitor <113518565+JersonVitor@users.noreply.github.com>
2024-10-03 21:40:24 -0300

    Delete SoundEyes directory

--- SoundEyes/app/src/main/java/com/jerson/soundeyes/feature_app/consts/Const.kt
+++
@@ -1,6 +0,0 @@
-package com.jerson.soundeyes.feature_app.consts
-
-object Const {
-    const val LABELS_YOLO = "labelsYolo.txt"
-    const val LABELS_MOBNET = "labelsMobileNet.txt"
-}
--- SoundEyes/app/src/main/java/com/jerson/soundeyes/feature_app/presentation/main/MainEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.jerson.soundeyes.feature_app.presentation.main
-
-sealed class MainEvent{
-    object NavigateToYolo: MainEvent()
-    object NavigateToMobile: MainEvent()
-    object PermissionCamera: MainEvent()
-    object PermissionGalery: MainEvent()
-}
